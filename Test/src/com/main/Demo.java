a8605741d4010393ca5026b475bb567c095e27c1
MukulDabare <106649658+MukulDabare@users.noreply.github.com>
2024-02-23 14:11:08 +0530

    kp

--- Test/src/com/main/Demo.java
+++ Test/src/com/main/Demo.java
@@ -1,13 +1,14 @@
 package com.main;
 
 public class Demo {
-	
-	public static void main(String[] args) {
-		
-       System.out.println("Hello");
-       System.out.println("In master");
-       System.out.println("Create Pull Resques");
-       System.out.println("Create PR");
-	}
+
+       public static void main(String[] args) {
+
+              System.out.println("Hello");
+              System.out.println("In master");
+              System.out.println("Create Pull Resques");
+              System.out.println("Create PR");
+              System.out.println("tp");
+       }
 
 }
